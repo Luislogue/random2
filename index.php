f8a3f109ac944e2f6e0dd45f936d1b99758eee9f
juliajaca <juliajaca@hotmail.com>
2019-07-10 12:31:14 +0200

    he intentado entender que habia hecho david, lo he entendido hasta la parte del unset, es lo que estaba probando ahora

--- index.php
+++ index.php
@@ -3,45 +3,49 @@
 $personas = ['toni','lolo','jordi','alexis','luis','alejeandro','adrian','fran','cristian','petro','julia','jorge','jose','jaume','mateo','david'];
 
 $parejas = count($personas)/2;
+// echo($parejas);
 
-for($i = 0; $i< $parejas; $i++){
+// for($i = 0; $i< $parejas; $i++){
 
-$primer_numero = mt_rand(0,count($personas));
+$contar = count($personas);
+// echo($contar);
+
+ $primer_numero = mt_rand(0,$contar) ; //
+ //echo $primer_numero;
 $primera_persona = $personas[$primer_numero];
-unset($parejas[$primer_numero]);
+//echo ($personas[0]);//
+//echo $primera_persona;
+echo $parejas;
+ unset($personas[$primer_numero]); // unset destruye la variable
 
-$segundo_numero = mt_rand(0,count($personas)-1);
-while($primer_numero == $segundo_numero){
+// $segundo_numero = mt_rand(0,count($personas)-1);
+// while($primer_numero == $segundo_numero){
 
-    $segunda_persona = mt_rand(0,count($personas)-1);
-}
+//     $segunda_persona = mt_rand(0,count($personas)-1);
+// }
 
 
-$segunda_persona = $personas[$segundo_numero];
-unset($parejas[$segundo_numero]);
+// $segunda_persona = $personas[$segundo_numero];
+// unset($parejas[$segundo_numero]);
     
 
-echo $primera_persona." ----->".$segunda_persona."<br>;
-}
-
-
-
-
+// echo $primera_persona." ----->".$segunda_persona."<br>";
+// };
 
 
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Random</title>
-</head>
-<body>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Random</title>
+ </head>
+ <body>
     
 
 
